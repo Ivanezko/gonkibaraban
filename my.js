e51133af41b6c6248910b79cb79a4f7b1a56914b
Ivanezko <ivanezko@gmail.com>
2013-04-18 00:49:34 +0300

    init

--- my.js
+++ my.js
@@ -1,36 +1,60 @@
 // Put your custom code here
 
 $().ready(function(){
-
+    log('DOM ready');
     document.addEventListener("deviceready", function() {
         document.addEventListener("online", function() {
-            alert('online!');
+            log('online!');
         }, false);
 
         document.addEventListener("offline", function() {
-            alert('offline!');
+            log('offline!');
         }, false);
 
         document.addEventListener("pause", function() {
-            alert('paused!');
+            log('paused!');
         }, false);
 
         document.addEventListener("resume", function() {
-            alert('resume!');
+            log('resume!');
         }, false);
 
         document.addEventListener("batterycritical", function() {
-            alert('batterycritical!');
+            log('batterycritical!');
         }, false);
 
         document.addEventListener("batterystatus", function(info) {
-            alert('batterystatus!'+info.level);
-            alert('batterystatus!'+info.isPlugged);
+            log('batterystatus!'+info.level);
+            log('batterystatus!'+info.isPlugged);
         }, false);
 
 
         // my events
+        $('.gn-number').on('click', function(e) {
+            log('click number:'+$(this).text());
+        });
+        $('.gn-space').on('click', function(e) {
+            log('click space');
+        });
+        $('.gn-submit').on('click', function(e) {
+            log('click submit');
+            $.getJSON('http://rally.co.ua/rallies/21/site/crew', function(data) {
+                log('json retrieved');
+                var items = [];
 
+                $.each(data, function(key, val) {
+                    items.push('<li id="' + key + '">' + val + '</li>');
+                });
+
+                $('<ul/>', {
+                    'class': 'my-new-list',
+                    html: items.join('')
+                }).appendTo('body');
+            });
+        });
+        $('.gn-clear').on('click', function(e) {
+            log('click clear');
+        });
 
 
 
@@ -39,3 +63,7 @@ $().ready(function(){
     }, false);
 
 });
+
+function log(string) {
+    $('#log').append('<p>'+string+'</p>');
+}
