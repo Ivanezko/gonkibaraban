7dc217b3452105fdd73cc350ef6d535f296f12c2
Ivanezko <ivanezko@gmail.com>
2013-04-18 00:51:01 +0300

    init

--- my.js
+++ my.js
@@ -28,39 +28,34 @@ $().ready(function(){
             log('batterystatus!'+info.isPlugged);
         }, false);
 
-
-        // my events
-        $('.gn-number').on('click', function(e) {
-            log('click number:'+$(this).text());
-        });
-        $('.gn-space').on('click', function(e) {
-            log('click space');
-        });
-        $('.gn-submit').on('click', function(e) {
-            log('click submit');
-            $.getJSON('http://rally.co.ua/rallies/21/site/crew', function(data) {
-                log('json retrieved');
-                var items = [];
-
-                $.each(data, function(key, val) {
-                    items.push('<li id="' + key + '">' + val + '</li>');
-                });
-
-                $('<ul/>', {
-                    'class': 'my-new-list',
-                    html: items.join('')
-                }).appendTo('body');
-            });
-        });
-        $('.gn-clear').on('click', function(e) {
-            log('click clear');
-        });
-
-
-
-
-
     }, false);
+
+    // my events
+    $('.gn-number').on('click', function(e) {
+        log('click number:'+$(this).text());
+    });
+    $('.gn-space').on('click', function(e) {
+        log('click space');
+    });
+    $('.gn-submit').on('click', function(e) {
+        log('click submit');
+        $.getJSON('http://rally.co.ua/rallies/21/site/crew', function(data) {
+            log('json retrieved');
+            var items = [];
+
+            $.each(data, function(key, val) {
+                items.push('<li id="' + key + '">' + val + '</li>');
+            });
+
+            $('<ul/>', {
+                'class': 'my-new-list',
+                html: items.join('')
+            }).appendTo('body');
+        });
+    });
+    $('.gn-clear').on('click', function(e) {
+        log('click clear');
+    });
 
 });
 
