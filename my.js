8dad85d15220cc5f07dc0882f97feabb5988f4ca
Ivanezko <ivanezko@gmail.com>
2013-04-18 00:32:55 +0300

    init

--- my.js
+++ my.js
@@ -1,1 +1,41 @@
 // Put your custom code here
+
+$().ready(function(){
+
+    document.addEventListener("deviceready", function() {
+        document.addEventListener("online", function() {
+            alert('online!');
+        }, false);
+
+        document.addEventListener("offline", function() {
+            alert('offline!');
+        }, false);
+
+        document.addEventListener("pause", function() {
+            alert('paused!');
+        }, false);
+
+        document.addEventListener("resume", function() {
+            alert('resume!');
+        }, false);
+
+        document.addEventListener("batterycritical", function() {
+            alert('batterycritical!');
+        }, false);
+
+        document.addEventListener("batterystatus", function(info) {
+            alert('batterystatus!'+info.level);
+            alert('batterystatus!'+info.isPlugged);
+        }, false);
+
+
+        // my events
+
+
+
+
+
+
+    }, false);
+
+});
