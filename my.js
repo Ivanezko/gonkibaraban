954b8eb4557a513bebcca5005ced3f4d062968fe
Ivanezko <ivanezko@gmail.com>
2013-04-18 23:23:32 +0300

    ~

--- my.js
+++ my.js
@@ -2,34 +2,6 @@
 
 $().ready(function(){
     log('DOM ready');
-    document.addEventListener("deviceready", function() {
-        log('device ready');
-        document.addEventListener("online", function() {
-            log('online!');
-        }, false);
-
-        document.addEventListener("offline", function() {
-            log('offline!');
-        }, false);
-
-        document.addEventListener("pause", function() {
-            log('paused!');
-        }, false);
-
-        document.addEventListener("resume", function() {
-            log('resume!');
-        }, false);
-
-        document.addEventListener("batterycritical", function() {
-            log('batterycritical!');
-        }, false);
-
-        document.addEventListener("batterystatus", function(info) {
-            log('batterystatus!'+info.level);
-            log('batterystatus!'+info.isPlugged);
-        }, false);
-
-    }, false);
 
     //log('device name:'+window.device.name);
 
@@ -68,3 +40,33 @@ function log(string) {
     $('#log').append('<p>'+string+'</p>');
     console.log(string);
 }
+
+
+document.addEventListener("deviceready", function() {
+    log('device ready');
+    document.addEventListener("online", function() {
+        log('online!');
+    }, false);
+
+    document.addEventListener("offline", function() {
+        log('offline!');
+    }, false);
+
+    document.addEventListener("pause", function() {
+        log('paused!');
+    }, false);
+
+    document.addEventListener("resume", function() {
+        log('resume!');
+    }, false);
+
+    document.addEventListener("batterycritical", function() {
+        log('batterycritical!');
+    }, false);
+
+    document.addEventListener("batterystatus", function(info) {
+        log('batterystatus!'+info.level);
+        log('batterystatus!'+info.isPlugged);
+    }, false);
+
+}, false);
