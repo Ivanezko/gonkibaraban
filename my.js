4c2a48700d293e3c23ccc0bbf04ac375b8c4122e
Ivanezko <ivanezko@gmail.com>
2016-05-22 05:15:16 +0300

    asd

--- my.js
+++ my.js
@@ -4,59 +4,102 @@ var uuid = document.location.hash.replace('#','');
 var mode = '';
 var rallyId = 0;
 var isRegistered = false;
+var queue = {}; // {info:info,date:new Date()}
+
+var auth_interval = 30000;
+var send_queue_interval = 7000;
+
+
+function sendQueue(info) {
+    if (typeof(info) == 'undefined') {
+        // wdo we have something old in queue to send?
+        var found = false;
+        $.each(queue, function( k, v ) {
+            if (!found && (new Date() - v.date > 30000) ) {
+                found = true;
+                sendData(v.info);
+            }
+        });
+    }
+    if (typeof(info) != 'undefined') {
+        sendData(info);
+    }
+}
+
+
+function sendData(info) {
+    queue[info.result] = {info:info, date:new Date()};
+    $.getJSON('http://gonki.in.ua/rallies/' + rallyId + '/site/mobileinput', info)
+        .done(function (data) {
+            if (data.result) {
+                delete queue[data.result];
+                log(data.result, 'data');
+            } else {
+                log(data.error);
+            }
+        })
+        .fail(function (jqxhr, textStatus, error) {
+            var err = textStatus + ', ' + error;
+            //log("нет ответа от сервера: " + err);
+        });
+}
+
+function register() {
+    $.getJSON('http://gonki.in.ua/', {act:'getRallyInfo'})
+        .done(function(data) {
+            $('#title').html(data.name);
+            rallyId = data.id;
+            isRegistered = true;
+            log('register ok:' + rallyId);
+            auth();
+            setInterval(auth, auth_interval);
+            setInterval(sendQueue, send_queue_interval);
+        })
+        .fail(function( jqxhr, textStatus, error ) {
+            var err = textStatus + ', ' + error;
+            log( "Register error: " + err);
+        });
+}
+
+function auth() {
+    if (!isRegistered) {
+        register();
+        return;
+    }
+    //log( "Auth try with uid: " + uuid);
+    $.getJSON('http://gonki.in.ua/rallies/' + rallyId + '/site/mobileinput', {act:'auth',uuid:uuid})
+        .done(function(data) {
+            $('#gnPname').html(data.n);
+            $('#gnInputtext').css('font-size','34px');
+            mode = data.m;
+            //log( "Auth ok: " + mode);
+            if (mode == 'finish') {
+                $('#gnInputtext').attr('placeholder', 'ББ-ЧЧММССмс');
+            }
+            if (mode == 'start') {
+                $('#gnInputtext').attr('placeholder', 'ББ-ЧЧММ');
+            }
+            if (mode == 'kv') {
+                $('#gnInputtext').attr('placeholder', 'ББ-ЧЧММ');
+            }
+            if (mode == 'full') {
+                $('#gnInputtext').attr('placeholder', 'ББ-ЧЧММ-ЧЧММССмс');
+                $('#gnInputtext').css('font-size','30px');
+            }
+        }).fail(function( jqxhr, textStatus, error ) {
+        var err = textStatus + ', ' + error;
+        log( "Auth error: " + err);
+    });
+}
+
+
+
+
+
 $().ready(function(){
     log('приложение запущено');
     log('version: 2014.02.08.1');
 
-    var auth_interval = 10000;
-
-    function register() {
-        $.getJSON('http://gonki.in.ua/', {act:'getRallyInfo'})
-            .done(function(data) {
-                $('#title').html(data.name);
-                rallyId = data.id;
-                isRegistered = true;
-                log('register ok:' + rallyId);
-            })
-            .fail(function( jqxhr, textStatus, error ) {
-                var err = textStatus + ', ' + error;
-                log( "Register error: " + err);
-            });
-    }
-
-    function auth() {
-        if (!isRegistered) {
-            register();
-            return;
-        }
-        //log( "Auth try with uid: " + uuid);
-        $.getJSON('http://gonki.in.ua/rallies/' + rallyId + '/site/mobileinput', {act:'auth',uuid:uuid})
-            .done(function(data) {
-                $('#gnPname').html(data.n);
-                $('#gnInputtext').css('font-size','34px');
-                mode = data.m;
-                //log( "Auth ok: " + mode);
-                if (mode == 'finish') {
-                    $('#gnInputtext').attr('placeholder', 'ББ-ЧЧММССмс');
-                }
-                if (mode == 'start') {
-                    $('#gnInputtext').attr('placeholder', 'ББ-ЧЧММ');
-                }
-                if (mode == 'kv') {
-                    $('#gnInputtext').attr('placeholder', 'ББ-ЧЧММ');
-                }
-                if (mode == 'full') {
-                    $('#gnInputtext').attr('placeholder', 'ББ-ЧЧММ-ЧЧММССмс');
-                    $('#gnInputtext').css('font-size','30px');
-                }
-            }).fail(function( jqxhr, textStatus, error ) {
-                var err = textStatus + ', ' + error;
-                log( "Auth error: " + err);
-            });
-    }
-
-    auth();
-    setInterval(auth, auth_interval);
 
     document.addEventListener("deviceready", function() {
         if (typeof device != 'undefined') uuid = device.uuid;
@@ -94,8 +137,9 @@ $().ready(function(){
 
     // my events asdsa
 
-    $('#gnSubmit').on('taphold', function(e) {
+    $('#gnSubmit').on('click', function(e) {
         var d = $('#gnInputtext').val();
+        $('#gnInputtext').val('');
 
         if (mode == 'finish' && !(/\d{1,3}[^\d]\d{8}/.test(d) || /\d{4}> \d{2}:\d{2}:\d{2},\d{2} .{2}/.test(d)) ) {
             alert('Ошибка ввода!'); return;
@@ -110,21 +154,7 @@ $().ready(function(){
             alert('Ошибка ввода!'); return;
         }
 
-        var info = {result:d, uuid:uuid};
-        $.getJSON('http://gonki.in.ua/rallies/' + rallyId + '/site/mobileinput', info)
-            .done(function(data) {
-                if (data.result) {
-                    log(data.result, 'data');
-                    $('#gnInputtext').val('');
-                } else {
-                    alert(data.error);
-                }
-
-            })
-            .fail(function(jqxhr, textStatus, error) {
-                var err = textStatus + ', ' + error;
-                log( "запрос не завершен: " + err);
-            });
+        sendQueue({result:d, uuid:uuid});
     });
 
 });
@@ -136,17 +166,4 @@ function log(string, type) {
         $('#log').prepend('<p class="log">'+string+'</p>');
 
     console.log(uuid + ':' + string);
-}
-
-function check_network() {
-    var networkState = navigator.network.connection.type;
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Неизвестное соединение';
-    states[Connection.ETHERNET] = 'кабельное соединение';
-    states[Connection.WIFI]     = 'WiFi соединение';
-    states[Connection.CELL_2G]  = '2G соединение';
-    states[Connection.CELL_3G]  = 'Cell 3G соединение';
-    states[Connection.CELL_4G]  = 'Cell 4G соединение';
-    states[Connection.NONE]     = 'Нет соединения с интернетом';
-    confirm('Вы подключены через:\n ' + states[networkState]);
 }
